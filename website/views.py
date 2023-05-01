d7569f628ef49708a442bf8620910b3d0b9375e9
Akash R Chandran <chandranrakash@gmail.com>
2023-05-02 02:11:05 +0530

    updated

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Package
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def package(request):
+    return HttpResponse('This is the package page.')
