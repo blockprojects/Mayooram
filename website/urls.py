b0ffe1f873b9bda71a4114a85799b29063171216
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 00:30:20 +0530

    added home and package route

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('package/', views.package, name='about'),
+    path('package/<int:package_id>/', views.package, name='about'),
 ]
