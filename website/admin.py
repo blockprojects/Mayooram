e5c4c126c920b2de19399c34a533f66f70d21006
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 00:29:55 +0530

    registered new models

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Package
+from .models import Package, Slide_Show_Images, Gallery_Images
 # Register your models here.
 
 admin.site.register(Package)
+admin.site.register(Slide_Show_Images)
+admin.site.register(Gallery_Images)
