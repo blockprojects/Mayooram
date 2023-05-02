bb7543dea47e180817ce282ff3d91f2646a1e138
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 00:30:38 +0530

    added rendering of index and package

--- website/views.py
+++ website/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from .models import Package
+from .models import Package, Slide_Show_Images, Gallery_Images
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    packages = Package.objects.all()
+    slideshow = Slide_Show_Images.objects.all()
+    gallery = Gallery_Images.objects.all()
+    for package in packages:
+        day, night = package.duration.split('/')
+        package.duration_txt = f'{day.replace("D","")} Days/{night.replace("N","")} Nights'
+    return render(request, 'index.html', {'packages': packages, 'slideshow': slideshow, 'gallery': gallery})
 
-def package(request):
-    return HttpResponse('This is the package page.')
+def package(request, package_id):
+    pack = Package.objects.get(id=package_id)
+    pack.pack_services = pack.pack_services.split('\n')
+    return render(request, 'package.html', {'package': pack})
