7269febc3ff3361e9871a7161bd6f798c2e8edd6
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 23:15:35 +0530

    updated views

--- website/views.py
+++ website/views.py
@@ -1,5 +1,14 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from django.conf import settings
 from .models import Package, Slide_Show_Images, Gallery_Images
+import razorpay
+import json
+from .email_config import enquiry_email, paid_email
+
+client = razorpay.Client(auth=(settings.RAZORPAY_ID, settings.RAZORPAY_SECRET))
+client.set_app_details({"title" : "House Boat", "version" : "v0.0.1"})
 # Create your views here.
 
 def home(request):
@@ -15,3 +24,41 @@ def package(request, package_id):
     pack = Package.objects.get(id=package_id)
     pack.pack_services = pack.pack_services.split('\n')
     return render(request, 'package.html', {'package': pack})
+
+@csrf_exempt
+def create_order_id(request):
+    if request.method == 'POST':
+        data = request.body.decode('utf-8')
+        data = json.loads(data)
+        amount = float(data['amount'])
+        order = client.order.create({
+            'amount': amount * 100, 
+            'currency': 'INR', 
+        })
+        return JsonResponse({
+            'success': True,
+            'order': order,
+        })
+
+@csrf_exempt
+def success(request):
+    if request.method == 'POST':
+        data = request.body.decode('utf-8')
+        payment_id = request.POST.get('razorpay_payment_id')
+        pay_details = client.payment.fetch(payment_id)
+        pakage_name = pay_details['notes']['package_name']
+        paid_email(pay_details['notes']['email'], "Payment Successful", pakage_name, pay_details['amount']/100, pay_details['notes']['name'], pay_details['notes']['phone'])
+        return render(request, 'success.html')
+    
+@csrf_exempt
+def enquiry(request):
+    if request.method == 'POST':
+        try:
+            data = request.body.decode('utf-8')
+            data = json.loads(data)
+            enquiry_email(data['subject'], data['message'])
+            return JsonResponse({
+            'status': "success",
+            })
+        except Exception as e:
+            return JsonResponse({"error":"true"})
