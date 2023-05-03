2290db10adc47ee63727d063dd9f6aa3d2d0b2b7
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 19:54:06 +0530

    updated settings

--- houseboat/settings.py
+++ houseboat/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://api.razorpay.com']
