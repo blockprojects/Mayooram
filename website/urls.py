8fd160fa5c74bc6acb551512b1a7c7a68c1b1a97
Akash R Chandran <chandranrakash@gmail.com>
2023-05-03 23:15:26 +0530

    updated urls

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('package/<int:package_id>/', views.package, name='about'),
+    re_path(r'^package/(?P<package_id>\d+)/?$', views.package, name='package description'),
+    path('create/order', views.create_order_id, name='create_order_id'),
+    path('contact', views.enquiry, name='contact'),
+    path('success', views.success, name='success'),
 ]
